5144cffefb87e5b621d9c9fa6b2506f0988533af
Daniel Lwetabe <dannylwe11@gmail.com>
2021-07-03 21:54:52 +0300

    simple golang application

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+const PORT = ":9001"
 func main() {
-	fmt.Println("hello world")
+	fmt.Printf("starting application on port %v", PORT)
+	r := mux.NewRouter()
+	r.HandleFunc("/health", landingPage)
+	r.HandleFunc("/hello", helloPage)
+	log.Fatal(http.ListenAndServe(PORT, r))
+}
+
+func helloWorld() string {
+	return "Hello World"
+}
+
+func health() map[string]bool {
+	return map[string]bool {"ok": true}
+}
+
+func landingPage(w http.ResponseWriter, r * http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(health())
+}
+
+func helloPage(w http.ResponseWriter, r * http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	response := map[string]interface{}{}
+	response["data"] = helloWorld()
+	json.NewEncoder(w).Encode(response)
 }
